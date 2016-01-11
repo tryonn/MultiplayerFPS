7874d78090500c19a35eea61d1a9c62abbaf4212
Simão Menezes <simaomenezesneto@gmail.com>
2016-01-11 18:16:19 -0300

    Implementacao do motor para o player, implementacao dos controles do player

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(PlayerMotor))]
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 5f;
+    [SerializeField]
+    private float lookSensitivity = 5f;
+
+    private PlayerMotor motor;
+
+    void Start()
+    {
+        motor = GetComponent<PlayerMotor>();
+    }
+
+    void Update()
+    {
+        // calcula o moviemnto e velocidade um vector 3d
+        float _xMove = Input.GetAxisRaw("Horizontal"); //(1,0,0) || (-1,0,0)
+        float _zMove = Input.GetAxisRaw("Vertical");//(0,0,1) || (0,0,-1)
+
+        Vector3 _moveHorizontal = transform.right * _xMove;
+        Vector3 _moveVertical = transform.forward * _zMove;
+
+        // movimento final
+        Vector3 _velocity = (_moveHorizontal + _moveVertical).normalized * speed;
+
+        //aplica movimento
+        motor.Move(_velocity);
+
+        // calcula a rotacao de um verctor 3d.
+        float _yRot = Input.GetAxisRaw("Mouse X");
+
+        Vector3 _rotation = new Vector3(0f, _yRot, 0f) * lookSensitivity;
+
+        //aplica rotacao
+        motor.Rotation(_rotation);
+
+        // calcula rotacao da camera no eixo Y
+        float _xCameraRotation = Input.GetAxisRaw("Mouse Y");
+
+        Vector3 _cameraRotation = new Vector3(_xCameraRotation, 0f,0f);
+
+        //aplica rotacao da camera
+        motor.CameraRotation(_cameraRotation);
+
+
+    }
+}
---
+++ Assets/Scripts/PlayerMotor.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerMotor : MonoBehaviour
+{
+    [SerializeField]
+    private Camera cam;
+
+    private Rigidbody _rigidbody;
+    private Vector3 velocity = Vector3.zero;
+    private Vector3 rotation = Vector3.zero;
+    private Vector3 cameraRotation = Vector3.zero;
+
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        PerformMovement();
+        PerformRotation();
+    }
+
+    private void PerformRotation()
+    {
+        _rigidbody.MoveRotation(_rigidbody.rotation * Quaternion.Euler(rotation));
+        
+        if (cam != null)
+        {
+            cam.transform.Rotate(-cameraRotation);
+        }
+    }
+
+    private void PerformMovement()
+    {
+        if (velocity != Vector3.zero)
+        {
+            _rigidbody.MovePosition(_rigidbody.position + velocity * Time.fixedDeltaTime);
+        }
+    }
+
+    // pega o movimento do verctor
+    internal void Move(Vector3 _velocity)
+    {
+        velocity = _velocity;
+    }
+
+    // pega a rotacao do verctor
+    internal void Rotation(Vector3 _rotation)
+    {
+        rotation = _rotation;
+    }
+
+    internal void CameraRotation(Vector3 _cameraRotation)
+    {
+        cameraRotation = _cameraRotation;
+    }
+}
