400117a20b6727811b9a2ba2d455898708f63b40
Simão Menezes <simaomenezesneto@gmail.com>
2016-01-12 08:22:01 -0300

    Networking Intro

---
+++ Assets/Scripts/PlayerSetup.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerSetup : NetworkBehaviour
+{
+    [SerializeField]
+    Behaviour[] componetsToDisable; //componentes para ser disabilitado
+
+    private Camera sceneCamera;
+
+    private void Start()
+    {
+        if (!isLocalPlayer)
+        {
+            for (int i = 0; i < componetsToDisable.Length; i++)
+            {
+                componetsToDisable[i].enabled = false;
+            }
+        } else
+        {
+            sceneCamera = Camera.main;
+            if (sceneCamera != null)
+            {
+                sceneCamera.gameObject.SetActive(false);
+            }
+        }
+    }
+
+    private void OnDisable()
+    {
+        if (sceneCamera != null)
+        {
+            sceneCamera.gameObject.SetActive(true);
+        }
+    }
+}
